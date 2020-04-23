c2c5be4a119cdd941a85cf673cf9c3ed05949677
李SH <lisonghua1023@163.com>
2020-04-23 11:47:26 +0800

    Initial commit

---
+++ com/lsh/Pet.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/23 11:31
+# @Author : LSH
+# @Version : 1.0.0
+from abc import ABCMeta, abstractmethod
+
+
+class Pet(object, metaclass=ABCMeta):
+    """宠物"""
+
+    def __init__(self, nickname):
+        self._nickname = nickname
+
+    @abstractmethod
+    def make_voice(self):
+        """发出声音"""
+        pass
+
+
+class Dog(Pet):
+    """狗"""
+
+    def make_voice(self):
+        print('%s: 汪汪汪...' % self._nickname)
+
+
+class Cat(Pet):
+    """猫"""
+
+    def make_voice(self):
+        print('%s: 喵...喵...' % self._nickname)
+
+
+def main():
+    pets = [Dog('旺财'), Cat('凯蒂'), Dog('大黄')]
+    for pet in pets:
+        pet.make_voice()
+
+
+if __name__ == '__main__':
+    main()
